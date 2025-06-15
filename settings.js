710b57af03e84e590b10f1e5a16d3affa9e49a03
Sayurami <sayuramihiranga4@gmail.com>
2025-06-15 10:00:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'v6pUTbwC#Lffy41nDdI_CgPVDavOxb6LgduNZB6ClzrSU3rQcu50' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '5jk0TARL#0a0T1NnWPeWqAgRPsZK74fnPI9T-8ObtstaT2dtcVeg' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
